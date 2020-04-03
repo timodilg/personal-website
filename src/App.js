e2a711c55bd8ed9d6f9521151f66585b4604433d
Timo Dilg <timo_dilg@yahoo.de>
2020-04-03 17:58:08 +0200

    Add content to main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
-import "./App.css";
-import profileImg from "./images/timo-profile-700px.png";
+import React from 'react'
+import './App.css'
+import profilePicture from './images/timo-profile-700px.png'
+import Github from './images/github.png'
+import Linkedin from './images/linkedin.png'
+import Xing from './images/xing.svg'
+import Mail from './images/mail.png'
 
 function App() {
   return (
     <main>
-      <div>
-        <img src="{profileImg}" alt="timo dilg" className="profilePicture" />
-        <h1>Timo Dilg</h1>
-        <p>Frontend Developer</p>
-        <p>Icons Profile einsetzen</p>
+      <div className="Profile">
+        <img src={profilePicture} alt="timo dilg" className="ProfilePicture" />
+        <h1 className="Name">Timo Dilg</h1>
+        <p className="WorkTitle">Frontend Developer</p>
+        <div className="IconContainer">
+          <a href="https://github.com/timodilg">
+            <img src={Github} alt="github logo" className="Icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/timo-dilg/">
+            <img src={Linkedin} alt="linkedin logo" className="Icon" />
+          </a>
+          <a href="https://www.xing.com/profile/Timo_Dilg/cv">
+            <img src={Xing} alt="xing logo" className="Icon" />
+          </a>
+          <a href="mailto:timo_dilg@yahoo.de">
+            <img src={Mail} alt="mail logo" className="Icon" />
+          </a>
+        </div>
       </div>
 
       <p className="WelcomeHeadline">
@@ -17,7 +34,7 @@ function App() {
         Germany.
       </p>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
